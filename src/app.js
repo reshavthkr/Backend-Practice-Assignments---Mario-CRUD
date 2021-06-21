c6d6a4231e33af033b6d721a159846eef0d2b283
Reshav Thakur <Reshavthkr@gmail.com>
2021-06-21 11:25:02 +0530

    Create the API's

--- src/app.js
+++ src/app.js
@@ -13,6 +13,61 @@ app.use(bodyParser.json())
 
 // your code goes here
 
+//get all marioChar
+app.get('/mario',async(req,res)=>{
+    const marioChar = await marioModel.find();
+    res.send(marioChar)
+})
+
+//getting character details with id
+app.get('/mario/:id', async(req,res)=>{
+    try{
+        const mariChar = await marioModel.findById(req.params.id);
+        res.send(mariChar);
+    }
+    catch(error){
+        res.status(400 ).send({message:error.message})
+    }
+})
+
+//posting data
+app.post('/mario', async(req,res)=>{
+    if(!req.body.name || !req.body.weight){
+        res.status(400).send({message: 'either name or weight is missing'})
+    }
+    let marioChar = new marioModel({
+        name:req.body.name,
+        weight:req.body.weight
+    })
+    let result = await marioChar.save();
+    res.status(201).send(result);
+})
+
+//updating the data
+app.patch('/mario/:id',async(req,res)=>{
+    
+    try{
+        const mariChar = await marioModel.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        if(!mariChar){
+            res.status(400).send({message:"Invalid Id"});
+        }
+        res.send(mariChar)
+    }  
+    catch(error){
+        res.status(400).send({message: error.message})
+    }  
+})
+
+//Deleting the data
+app.delete('/mario/:id',async(req,res)=>{
+    try{
+        const mariChar = await marioModel.findByIdAndDelete(req.params.id)
+        res.send({message: 'character deleted'})
+    }
+    catch(error){
+        res.status(400).send({message: error.message})
+    }
+})
 
 
 module.exports = app;
--- src/models/marioChar.js
+++ src/models/marioChar.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 
 //  Your code goes here
+const marioModel = mongoose.model("mariochar", mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    weight:{
+        type:Number,
+        required:true
+    }
+    
+})
+)
+
 
 
 module.exports = marioModel;
